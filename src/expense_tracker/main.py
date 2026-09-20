7cdad24d773ff70226c97585cd4df914d7651174
rakesh <rp.code.rakesh@gmail.com>
2026-09-20 11:05:34 +0530

    show all transactions completed

--- src/expense_tracker/main.py
+++ src/expense_tracker/main.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from dataclasses import asdict
 
 from rich.console import Console
+from rich.table import Table
+from rich.text import Text
 
 from expense_tracker.config import TRANSACTIONS_FILE
 from expense_tracker.models.transaction import Transaction
@@ -32,20 +34,51 @@ def main():
 
         match choice:
             case 1:
+                # Adding Transactions
                 transactions = service.get_transactions()
 
                 id = get_next_id(transactions)
-                name = input("Enter Name ")
+                type = input("Enter Type ")
                 amount = float(input("Enter Amount "))
                 category = input("Enter category (Food, Shopping, Home expense, rent) ")
                 description = input("Enter description (rent for august etc.) ")
                 created_at = datetime.now()
 
-                transaction = Transaction(id, name, amount, category, description, created_at)
+                transaction = Transaction(id, type, amount, category, description, created_at)
                 service.add_transaction(transaction)
 
             case 2:
-                print("2....")
+                # Listing all transactions on console
+                transactions = service.get_transactions()
+
+                table_title = Text("List of transactions", style="bold dodger_blue1")
+                table = Table(title=table_title, style="bold gray50")
+                table.add_column("ID", style=" gray70")
+                table.add_column("TYPE", style=" gray70")
+                table.add_column("AMOUNT", style=" gray70")
+                table.add_column("CATEGORY", style=" gray70")
+                table.add_column("DESCRIPTION", style=" gray70")
+                table.add_column("CREATED AT", style=" gray70")
+
+                for transaction in transactions:
+
+                    if transaction.type == "expense":
+                        text = Text.assemble(str(transaction.amount), style="red")
+                    elif transaction.type == "income":
+                        text = Text.assemble(str(transaction.amount), style="green")
+
+                    table.add_row(
+                        str(transaction.id), 
+                        str(transaction.type), 
+                        text,
+                        str(transaction.category),
+                        str(transaction.description),
+                        str(transaction.created_at),
+                    )
+
+                console.print(table)
+
+
             case 'q':
                 console.print("\n\tSee you soon!", style="bold dodger_blue1")
                 print()
